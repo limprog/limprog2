6a8650b493d1f36e7a6cd353a7236ae9ea8aca00
Limprog <ilinivan1602@gmail.com>
2023-05-18 19:23:38 +0300

    оно работает

--- new_file.py
+++ new_file.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, Input, Output
 import plotly.express as px
 import pandas as pd
 
@@ -9,8 +9,28 @@ fig = px.scatter(data, x="Age", y="Tenure", color="Gender")
 
 app.layout = html.Div(children=[
     html.H1(children='ВОзраст и время'),
-    dcc.Graph(figure=fig)
+    dcc.Graph(figure={}, id="figure_1"),
+    'X',
+    dcc.Dropdown(
+        ["Age", "Tenure", "Balance", "EstimatedSalary"],
+        'Age', id='X'
+    ),
+    'Y',
+    dcc.Dropdown(
+        ["Age", "Tenure", "Balance", "EstimatedSalary"],
+        'Balance', id="Y"
+    ),
 ])
+
+@app.callback(
+    Output('figure_1', component_property='figure'),
+    Input('X', 'value'),
+    Input('Y', 'value')
+)
+def update_store_data(X, Y):
+    fig = px.scatter(data, x=X, y=Y, color="Gender")
+    return fig
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
